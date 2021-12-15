45ae9525c385e4b8564ef3741e7129ff5abc1382
Joshua "jag" Ginsberg <jag@tackle.io>
2021-12-15 15:37:28 -0500

    Fixes to tests.

--- woodchipper/processors.py
+++ woodchipper/processors.py
@@ -1,13 +1,12 @@
-import json
 from woodchipper import context
 
+
 def inject_context_processor(event_dict: dict):
-    """ 
+    """
     A function to update context with new log event key/value
     Returns updated context items
     """
-    ctx_items = dict(context.logging_ctx)
-    print(ctx_items)
+    ctx_items = context.logging_ctx.as_dict()
     ctx_items.update(event_dict)
 
     return ctx_items
