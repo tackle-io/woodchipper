04a064c2d281ec94e2f3e326a1705796c377d221
Joshua "jag" Ginsberg <jag@tackle.io>
2021-12-16 15:40:18 -0500

    Git revision logging processor

--- woodchipper/processors.py
+++ woodchipper/processors.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from woodchipper import context
@@ -12,3 +13,21 @@ def inject_context_processor(logger: logging.Logger, method: str, event_dict: di
     ctx_items.update(event_dict)
 
     return ctx_items
+
+
+class GitVersionProcessor:
+    """
+    Loads the contents of a static file written during CI builds with the git revision info.
+    """
+
+    def __init__(self, version_json_path="version.json"):
+        self.version_json_path = version_json_path
+
+    def __call__(self, logger: logging.Logger, method: str, event_dict: dict):
+        try:
+            with open(self.version_json_path) as ifs:
+                git_version = json.loads(ifs.read())
+                event_dict.update({f"git.{k}": v for k, v in git_version.items()})
+        except (OSError, json.JSONDecodeError):
+            pass
+        return event_dict
