92a55615fff84ad53537904e92066989f177b728
Joshua "jag" Ginsberg <jag@tackle.io>
2021-12-16 15:06:28 -0500

    Implement monitors. Modify woodchipper.configure.

---
+++ tests/test_sqlalchemy.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+
+import woodchipper
+from woodchipper.configs import DevLogToStdout
+from woodchipper.context import LoggingContext
+from woodchipper.monitors.sqlalchemy import SQLAlchemyMonitor
+
+engine = sqlalchemy.create_engine("sqlite:///:memory:")
+
+
+def connect(sa_monitor):
+    sa_monitor.engine = engine
+
+
+SQLAlchemyMonitor.instance_setup_cb = connect
+woodchipper.configure(DevLogToStdout, {"": "INFO"}, monitors=[SQLAlchemyMonitor])
+
+
+def test_sqlalchemy():
+    logger = woodchipper.get_logger(__name__)
+    with LoggingContext({"test": "sqlalchemy"}):
+        with engine.connect() as conn:
+            rows = conn.execute("SELECT 1")
+            logger.info("SQL result.", row=rows.fetchone())
+    # FIXME: Actually test something here
