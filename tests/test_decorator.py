b405421d2d216075ff3e4cd42982c576e0932a0e
Jonathan Demirgian <jd@Jupiter.local>
2021-12-15 19:20:56 -0500

    Add tests and make pluck_value public.

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,6 +1,9 @@
+from types import SimpleNamespace
 from unittest.mock import patch
 
-from woodchipper.decorator import arg_logger, missing
+import pytest
+
+from woodchipper.decorator import arg_logger, missing, pluck_value
 
 
 def test_arg_logger_invokes_logging_context_with_arguments():
@@ -16,3 +19,29 @@ def test_arg_logger_invokes_logging_context_with_arguments():
     assert mocked.call_args.args == (
         {"Bar": "bar value", "NestedObjID": "some_id", "NestValue": missing, "Foo": missing},
     )
+
+
+@pytest.mark.parametrize(
+    argnames=[
+        "obj",
+        "path_name",
+        "delimiter",
+        "return_value",
+    ],
+    argvalues=[
+        ({"foo": "bar"}, "foo", ".", "bar"),
+        ({"foo": {"baz": "quux"}}, "foo", ".", {"baz": "quux"}),
+        ({"foo": {"baz": {"quux": 42}}}, "foo.baz.quux", ".", 42),
+        ({"foo": "nope"}, "baz", ".", missing),
+        ({"foo": {"baz": {"nope": 42}}}, "foo.baz.quux", ".", missing),
+        ({"foo": SimpleNamespace(foo=42)}, "foo.foo", ".", 42),
+        ({"foo": SimpleNamespace(foo={"baz": "bar"})}, "foo.foo.baz", ".", "bar"),
+        ({"foo": SimpleNamespace(foo={"baz": "bar"})}, "foo.quux.baz", ".", missing),
+        ({"bar": SimpleNamespace()}, "foo", ".", missing),
+        ({"foo": SimpleNamespace(foo={"b.a.z": "bar"})}, "foo#foo#b.a.z", "#", "bar"),
+        ({"f.o.o": SimpleNamespace(foo={"baz": "bar"})}, "f.o.o#foo#baz", "#", "bar"),
+        ({"bar": SimpleNamespace()}, "bar", "#", SimpleNamespace()),
+    ],
+)
+def test_pluck_value(obj, path_name, delimiter, return_value):
+    assert pluck_value(obj, path_name, delimiter=delimiter) == return_value
